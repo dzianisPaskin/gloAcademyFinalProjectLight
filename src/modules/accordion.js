d97135550c1eab7f42a8c39ba348d258b91eeb29
Dzianis <paskin.denis.00gmail.com>
2022-08-11 23:42:14 +0200

    fixed the accordion

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -3,16 +3,19 @@ export const accordion = () => {
   const elem = accordion.querySelectorAll(".element");
   const elemContent = accordion.querySelectorAll(".element-content");
 
-
   accordion.addEventListener("click", (e) => {
     const target = e.target.closest(".element");
 
-    if (target) {
+    if (!target.classList.contains("active")) {
       elem.forEach((el, ind) => {
         if (el === target) {
           elemContent[ind].style.display = "block";
           el.classList.add("active");
-        } else {
+        }
+      });
+    } else {
+      elem.forEach((el, ind) => {
+        if (el === target) {
           el.classList.remove("active");
           elemContent[ind].style.display = "none";
         }
